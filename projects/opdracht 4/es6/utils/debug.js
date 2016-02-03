6c98d7d00793462b2f52f2f3c826f23e4bb5777c
dvens <d.joelvens@gmail.com>
2016-02-03 11:43:36 +0100

    bug fix

--- projects/opdracht 4/es6/utils/debug.js
+++ projects/opdracht 4/es6/utils/debug.js
@@ -8,20 +8,20 @@ debugUtils.debugId = false;
 
 debugUtils.geoErrorHandler = function(code, message) {
 
-	debugUtils.debugMessage('geo.js error ' + code + ': ' + message);
+	this.debugMessage('geo.js error ' + code + ': ' + message);
 
 }
 
 debugUtils.debugMessage = function() {
 
-	(debugUtils.customDebugging && debugUtils.debugId)?document.getElementById(debugId).innerHTML:console.log(message);
+	(this.customDebugging && debugUtils.debugId)?document.getElementById(debugId).innerHTML:console.log(message);
 
 }
 
 debugUtils.setCustomDebugging = function(id) {
 
-	debugUtils.debugId = this.debugId;
-	debugUtils.debugId = true;
+	this.debugId = this.debugId;
+	this.debugId = true;
 
 }
 
